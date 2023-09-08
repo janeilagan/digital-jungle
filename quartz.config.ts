66cf034532df043780b45a47a6b89bb8743ffe7e
Jane Ilagan <jane-ilagan@outlook.com>
2023-09-08 09:28:14 +0800

    personalize

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,31 +3,29 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "digital jungle 🍃",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    analytics: null,
+    baseUrl: "github.io/digital-jungle",
+    ignorePatterns: ["private", "templates", ".obsidian", "work"],
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Quicksand",
+        body: "Quicksand",
+        code: "Ubuntu Mono",
       },
       colors: {
         lightMode: {
           light: "#faf8f8",
           lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          gray: "#595959",
+          darkgray: "#2e2d2d",
+          dark: "#103302",
+          secondary: "#354f52",
+          tertiary: "#527496f",
+          highlight: "rgba(202,210,197, 0.15)",
         },
         darkMode: {
           light: "#161618",
@@ -35,9 +33,9 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#84a98c",
+          tertiary: "#527496f",
+          highlight: "rgba(202,210,197, 0.15)",
         },
       },
     },
